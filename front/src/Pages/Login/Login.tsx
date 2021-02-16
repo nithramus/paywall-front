d38816d1b359bdb9e719b858b4841e69700909f2
Baptiste Andre <bandre@profield.io>
2021-02-16 19:04:18 +0100

    deployments

--- front/src/Pages/Login/Login.tsx
+++ front/src/Pages/Login/Login.tsx
@@ -2,32 +2,53 @@ import { type } from "os";
 import React from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import "./Login.css";
+import * as yup from "yup";
+import { Formik } from "formik";
+
+let schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+});
 
 export default function Login() {
   return (
-    <Row style={{ height: "80vh" }}>
-      <Col className="mx-auto my-auto" style={{ maxWidth: "600px" }}>
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
+    <Formik
+      validationSchema={schema}
+      onSubmit={console.log}
+      initialValues={{
+        email: "Mark",
+      }}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        touched,
+        isValid,
+        errors,
+      }) => (
+        <Form noValidate onSubmit={handleSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} md="4" controlId="validationFormik01">
+              <Form.Label>email</Form.Label>
+              <Form.Control
+                type="text"
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+                // isValid={touched.email && !errors.email}
+                isInvalid={!!errors.email}
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback>
+              <Button type="submit">Submit form</Button>
+            </Form.Group>
+          </Form.Row>
         </Form>
-      </Col>
-    </Row>
+      )}
+    </Formik>
   );
 }
